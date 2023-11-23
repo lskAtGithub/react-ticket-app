1e2ed13962b7dfa93caa396d07cadfb534fb5662
liushengkai <949919743@qq.com>
2023-11-24 02:36:47 +0800

    feat: 云平台上传

--- src/app/components/PublishButton.tsx
+++ src/app/components/PublishButton.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import {
   Button,
   useDisclosure,
@@ -11,14 +13,45 @@ import {
   Input,
   Chip,
 } from '@nextui-org/react'
-import { Send } from 'lucide-react'
+import { FileUp, Send } from 'lucide-react'
+import { CldUploadButton } from 'next-cloudinary'
 import { useState } from 'react'
+import { request } from '@/app/utils/request'
+
+import type { CldUploadWidgetResults } from 'next-cloudinary'
+import { useAuth, useUser } from '@clerk/nextjs'
 
 function PublishButton() {
   const { isOpen, onOpen, onOpenChange } = useDisclosure()
   const [content, setContent] = useState('')
-  const [options, setOptions] = useState<String[]>([])
+  const [options, setOptions] = useState<string[]>([])
   const [currentOption, setCurrentOption] = useState('')
+  const [images, setImages] = useState<string[]>([])
+  const { userId } = useAuth()
+  const avatar = useUser().user?.imageUrl
+
+  const onSubmit = (onClose: Function) => {
+    request('/topic', {
+      method: 'POST',
+      body: JSON.stringify({
+        userId,
+        avatar,
+        content,
+        images,
+        options,
+      }),
+    }).then((res) => {
+      console.log(res)
+      onClose()
+      onReset()
+    })
+  }
+  const onReset = () => {
+    setContent('')
+    setOptions([])
+    setCurrentOption('')
+    setImages([])
+  }
 
   function Chips() {
     const onCloseOption = (current: String) => {
@@ -30,6 +63,34 @@ function PublishButton() {
         {item}
       </Chip>
     ))
+  }
+
+  function Upload() {
+    const onUploadSuccess = (result: CldUploadWidgetResults) => {
+      if (result.event === 'success' && result.info) {
+        setImages([...images, (result.info as any).secure_url as string])
+      }
+    }
+
+    return (
+      <>
+        <CldUploadButton
+          uploadPreset='ksvontss'
+          onSuccess={(result) => onUploadSuccess(result)}>
+          <button className='bg-secondary-400 hover:bg-secondary-700 text-white font-bold py-2 px-4 rounded'>
+            <div className='flex'>
+              <FileUp />
+              <span className='ml-2'>上传图片</span>
+            </div>
+          </button>
+        </CldUploadButton>
+        <div className=''>
+          {images.map((item) => {
+            return <img src={item} key={item} alt='' />
+          })}
+        </div>
+      </>
+    )
   }
 
   return (
@@ -54,6 +115,8 @@ function PublishButton() {
                   value={content}
                   onValueChange={setContent}></Textarea>
                 <Spacer x={2} />
+                {Upload()}
+                <Spacer x={2} />
                 <div className='flex items-center'>
                   <Input
                     label='输入选项'
@@ -71,15 +134,21 @@ function PublishButton() {
                     }}>
                     添加
                   </Button>
-                  <Spacer x={2} />
                 </div>
+                <Spacer x={2} />
                 <div className='flex gap-2 flex-wrap'>{Chips()}</div>
               </ModalBody>
               <ModalFooter>
-                <Button color='danger' variant='light' onPress={onClose}>
+                <Button
+                  color='danger'
+                  variant='light'
+                  onPress={() => {
+                    onClose()
+                    onReset()
+                  }}>
                   取 消
                 </Button>
-                <Button color='primary' onPress={onClose}>
+                <Button color='primary' onPress={() => onSubmit(onClose)}>
                   确 认
                 </Button>
               </ModalFooter>
